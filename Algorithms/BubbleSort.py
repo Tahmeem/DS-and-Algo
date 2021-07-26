84dd5c99d37ade67a9ae50d7fd36f74dd42a40d4
Tahmeem <tahmeemmahedi@gmail.com>
2021-07-25 21:58:22 -0400

    Algorithms added

---
+++ Algorithms/BubbleSort.py
@@ -0,0 +1,10 @@
+arr = [64,34,25,12,22,11,90]
+
+swap = False
+while not swap:
+	swap = True
+	for i in range(len(arr)-1):
+		if arr[i] > arr[i+1]:
+			swap = False
+			arr[i],arr[i+1] = arr[i+1],arr[i]
+print(arr)
